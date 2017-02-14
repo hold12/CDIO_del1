9f25b846e566f28f80afb3c603457fc0a0c08d95
Freya Helstrup <freyagh@me.com>
2017-02-14 16:00:39 +0100

    Reformatted code and added cpr and password attributes.

--- src/dto/User.java
+++ src/dto/User.java
@@ -7,13 +7,14 @@ import java.util.List;
  * Created by tjc on 9/2/17.
  */
 
-public class User{
+public class User {
 
-    private int	userId;
+    private int userId;
     private String userName;
     private String ini;
     private List<String> roles;
-    //TODO Add cpr + password
+    private String cpr;
+    private String password;
 
     public User() {
         this.roles = new ArrayList<>();
@@ -22,38 +23,60 @@ public class User{
     public int getUserId() {
         return userId;
     }
+
     public void setUserId(int userId) {
         this.userId = userId;
     }
+
     public String getUserName() {
         return userName;
     }
+
     public void setUserName(String userName) {
         this.userName = userName;
     }
+
     public String getIni() {
         return ini;
     }
+
     public void setIni(String ini) {
         this.ini = ini;
+    }
+
+    public String getCpr() {
+        return cpr;
+    }
+
+    public void setCpr(String cpr) {
+        this.cpr = cpr;
+    }
+
+    public String getPassword() {
+        return password;
+    }
+
+    public void setPassword(String password) {
+        this.password = password;
     }
 
     public List<String> getRoles() {
         return roles;
     }
+
     public void setRoles(List<String> roles) {
         this.roles = roles;
     }
 
-    public void addRole(String role){
+    public void addRole(String role) {
         this.roles.add(role);
     }
+
     /**
-     *
      * @param role
      * @return true if role existed, false if not
      */
-    public boolean removeRole(String role){
+    public boolean removeRole(String role) {
         return this.roles.remove(role);
     }
 
@@ -61,7 +84,6 @@ public class User{
     public String toString() {
         return "User [userId=" + userId + ", userName=" + userName + ", ini=" + ini + ", roles=" + roles + "]";
     }
-
 
 
 }

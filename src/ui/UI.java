c4b7beed4315abc07a990d8ba74cd86c0d720238
leiszner <candypanda1012>
2017-02-16 16:13:26 +0100

    added content to interface ui

--- src/ui/UI.java
+++ src/ui/UI.java
@@ -1,7 +1,18 @@
 package ui;
 
+import dto.User;
+
 /**
  * Created by freya on 14-02-2017.
  */
-public interface UI {
+public interface UI
+{
+    void printWelcomeMsg();
+    void printMsg(String message);
+    void listUsers();
+    void closeProgram();
+    String getUserInput();
+    User createUser();
+    User editUser(User user);
+    boolean removeUser(User user);
 }

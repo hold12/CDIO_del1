0af34dbddec69dca941dcf3f5704baba75e18e76
leiszner <candypanda1012>
2017-02-16 22:24:01 +0100

    made temp fix in DBTester

--- src/DBTester.java
+++ src/DBTester.java
@@ -15,7 +15,8 @@ public class DBTester {
     public static void main(String[] args) {
         // TODO: Has been set yo null to get rid of compile issues
         IUserAdministration iDAO = null/*new UserDAODiscImpl()*/;
-        User newUser = new User();
+        //TODO Give newUser constructor parameters
+        User newUser = new User(-1, null, null, null, null, null);
 
         /**
          * Things to test for:
@@ -28,7 +29,8 @@ public class DBTester {
         newUser.setInitials("test");
         newUser.addRole("Admin");
         newUser.setUserName("testName");
-        newUser.setUserId(0);
+        //TODO Rewrite
+        //newUser.setUserId(0);
         try {
             iDAO.createUser(newUser);
         } catch (DataAccessException e) {
@@ -41,7 +43,8 @@ public class DBTester {
             System.out.println("User already existed - OK");
         }
 
-        newUser.setUserId(1);
+        //TODO Rewrite
+        //newUser.setUserId(1);
         newUser.setUserName("2ND user");
         try {
             iDAO.createUser(newUser);
@@ -49,7 +52,8 @@ public class DBTester {
             e1.printStackTrace();
         }
         printUsers(iDAO);
-        newUser.setUserId(0);
+        //TODO Rewrite
+        //newUser.setUserId(0);
         newUser.setUserName("ModifiedName");
         try {
             iDAO.updateUser(newUser);

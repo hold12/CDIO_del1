6413deab9bfca0c37abe140825204c18d2e820e8
Freya Helstrup <freyagh@me.com>
2017-02-17 23:17:57 +0100

    Added Password class and changed methods in User using password variable

---
+++ src/dto/Password.java
@@ -0,0 +1,20 @@
+package dto;
+
+/**
+ * Created by freya on 17-02-2017.
+ */
+public class Password {
+    private String password;
+
+    public Password() {
+        //TODO: Generate password and save in variable password
+    }
+
+    public String getPassword() {
+        return password;
+    }
+
+    public void setPassword(String password) {
+        this.password = password;
+    }
+}
--- src/dto/User.java
+++ src/dto/User.java
@@ -13,15 +13,15 @@ public class User {
     private String ini;
     private List<String> roles;
     private String cpr;
-    private String password;
+    private Password password;
 
-    public User(int userId, String userName, String ini, List<String> roles, String cpr, String password) {
+    public User(int userId, String userName, String ini, List<String> roles, String cpr) {
         this.userId = userId;
         this.userName = userName;
         this.ini = ini;
         this.roles = roles;
         this.cpr = cpr;
-        this.password = password;
+        this.password = new Password();
     }
 
     public int getUserId() {
@@ -49,11 +49,11 @@ public class User {
     }
 
     public String getPassword() {
-        return password;
+        return this.password.getPassword();
     }
 
     public void setPassword(String password) {
-        this.password = password;
+        this.password.setPassword(password);
     }
 
     public List<String> getRoles() {

3aae72943bb5a985313d42518695083d3dd5cc8c
Anders W. Olsen <anders@wiberg.tech>
2017-02-19 18:34:45 +0100

    Basic foundation for the UserAdministrationDB's getUsers method.

--- src/dal/UserAdministrationDB.java
+++ src/dal/UserAdministrationDB.java
@@ -2,12 +2,21 @@ package dal;
 
 import dto.User;
 
+import java.sql.ResultSet;
+import java.sql.SQLException;
+import java.util.ArrayList;
 import java.util.List;
 
 /**
  * Created by freya on 14-02-2017.
  */
 public class UserAdministrationDB implements IUserAdministration {
+    private DBConnection dbConnection;
+
+    public UserAdministrationDB(DBConnection dbConnection) {
+        this.dbConnection = dbConnection;
+    }
+
     @Override
     public User getUser(int userId) throws DataAccessException {
         return null;
@@ -15,7 +24,40 @@ public class UserAdministrationDB implements IUserAdministration {
 
     @Override
     public List<User> getUserList() throws DataAccessException {
+        List<User> users = new ArrayList<>();
+        ResultSet resultSet = dbConnection.query("SELECT * FROM User");
+
+        try {
+            while(resultSet.next()) {
+                users.add(getUserFromResultSet(resultSet));
+            }
+        } catch(SQLException e) {
+            System.out.println("[UserAdministrationDB]: An error occured when trying to fetch all users from the database.");
+            e.printStackTrace();
+            return null;
+        } finally {
+            dbConnection.close();
+        }
+
         return null;
+    }
+
+    private User getUserFromResultSet(ResultSet resultSet) throws SQLException {
+        int userId = 0;
+
+        try {
+            Integer.parseInt(resultSet.getString("ID")); // TODO: Make sure all fields are spelled correctly
+        } catch(Exception e) {
+            System.out.println("Invalid datatype for user id.");
+        }
+
+        String userName    = resultSet.getString("userName");
+        String ini         = resultSet.getString("ini"); // TODO: Refactered to initials?
+        List<String> roles = new ArrayList<>(); // TODO: Make a list here
+        String cpr         = resultSet.getString("CPR");
+        String password    = resultSet.getString("password");
+
+        return new User(userId, userName, ini, roles, cpr, password);
     }
 
     @Override

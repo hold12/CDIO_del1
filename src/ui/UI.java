039c3a291b4bddb15dcf5ba18e540fd7e49c5e37
awo-dev <!9HdGbpfM?>
2017-02-23 12:56:47 +0100

    Cleaning up UI interface

--- src/ui/UI.java
+++ src/ui/UI.java
@@ -1,11 +1,5 @@
 package ui;
 
-import dal.UserAdministrationDB;
-import dto.User;
-
-import java.io.IOException;
-import java.util.List;
-
 /**
  * Wrote by Kasper on 14-02-2017.
  */
@@ -15,11 +9,4 @@ public interface UI
     String getUserInput(String message);
     void printMsg(String message);
     void showRoles(String[] roles);
-
-    // TODO: Methods below should be removed.
-//    void printMainMenu() throws IOException;
-//    void listUsers(List<User> listOfUsers);
-//    void removedUserMsg(User user);
-//    User editUser(User user);
-//    User createUser();
 }
